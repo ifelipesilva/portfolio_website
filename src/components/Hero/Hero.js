51fa495dad3fd8138a1f916c1fba8bebbe5ca576
feehlipeeh <feehlipeeh422@gmail.com>
2021-11-15 11:44:41 -0300

    hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React from 'react'
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle
+} from '../../styles/GlobalComponents'
+import Button from '../../styles/GlobalComponents/Button'
+import { LeftSection, Span } from './HeroStyles'
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
-);
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello. <br />
+        I am Felipe Silva <br />
+        <Span>Developer UX/UI Designer Freelance</Span>
+      </SectionTitle>
+      <SectionText>
+        I am a developer and UX/UI designer based in Brazil.
+      </SectionText>
+      <Button
+        onClick={() => (window.location = 'mailto:feehlipeeh422@gmail.com')}
+      >
+        Get in touch
+      </Button>
+    </LeftSection>
+  </Section>
+)
 
-export default Hero;
+export default Hero
--- src/components/Hero/HeroStyles.js
+++ src/components/Hero/HeroStyles.js
@@ -1,19 +1,25 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 export const LeftSection = styled.div`
   width: 100%;
-  @media ${(props) => props.theme.breakpoints.sm} {
+  @media ${props => props.theme.breakpoints.sm} {
     width: 80%;
     display: flex;
     flex-direction: column;
 
     margin: 0 auto;
   }
-  @media ${(props) => props.theme.breakpoints.md} {
+  @media ${props => props.theme.breakpoints.md} {
     width: 100%;
     display: flex;
     flex-direction: column;
 
     margin: 0 auto;
   }
-`;
+`
+
+export const Span = styled.span`
+  display: flex;
+  justify-content: space-evenly;
+  font-size: 2rem;
+`
